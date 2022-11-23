4a1b6f17a52f8899f00b9f786ddbfe65a76aced5
hamsa <hafieekhan@gmail.com>
2022-11-23 06:26:40 +0300

    Completed Lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,7 @@
 class StudentsController < ApplicationController
 
   def index
-    students = if params[:name]
-      Student.by_name(params[:name])
-    else
-      Student.all
-    end
+    students = Student.all
     render json: students
   end
   def show
